07de3576ff5a6415a5622073da70e70adb0107b0
jinvvoo <jinvvookim@gmail.com>
2023-06-22 16:13:26 +0900

    start!

---
+++ src/main/java/com/example/gitflow/Prac1GitFlowApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prac1GitFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Prac1GitFlowApplication.class, args);
+    }
+
+}
