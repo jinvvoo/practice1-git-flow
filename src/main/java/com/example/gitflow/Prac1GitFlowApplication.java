9affed58ffe4ca40d300d335e150aa54588cb20e
jinvvoo <jinvvookim@gmail.com>
2023-06-22 16:28:59 +0900

    start!

--- src/main/java/com/example/gitflow/Prac1GitFlowApplication.java
+++ src/main/java/com/example/gitflow/Prac1GitFlowApplication.java
@@ -8,6 +8,10 @@ public class Prac1GitFlowApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Prac1GitFlowApplication.class, args);
+		System.out.println("feature/memberDTO생성");
+		System.out.println("feature/memberDTO생성");
+
+
 	}
 
 }
